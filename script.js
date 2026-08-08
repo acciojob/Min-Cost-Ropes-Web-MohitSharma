b3a0ee151a34679ef12f958f0e5793c8e421f350
Mohit Sharma <skmohit9955@gmail.com>
2026-08-08 14:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	let minRope = 0;
+	while (arr.length >= 2){
+		arr.sort((a,b)=> a-b)
+
+		let first  = arr.shift();
+		let second = arr.shift();
+
+		let sum = first + second
+		minRope += sum
+
+		arr.push(sum)
+	}
+	return minRope;
 }
 
 module.exports=mincost;
